b14d1b7e77fdd9d48c1525167be9aee3963a9c49
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-03-21 06:45:28 +0700

    excercise Array

---
+++ lab13/Array.js
@@ -0,0 +1,94 @@
+// Tính tổng các phần tử trong mảng số
+function sumOfArray(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Tính trung bình cộng các số trong một mảng
+function averageOfArray(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Tìm số lớn nhất trong mảng số
+// Giả sử mảng chưa được sắp xếp và mỗi giá trị chỉ xuất hiện 1 lần
+function findMax(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Tìm số nhỏ nhất trong 1  mảng số
+// Giả sử mảng chưa được sắp xếp và mỗi giá trị chỉ xuất hiện 1 lần
+function findMin(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm kiểm tra xem một mảng số có phải mảng tăng dần hay không
+// Mảng tăng dần nghĩa là các phần tử có giá trị tăng dần
+function isAscending(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm kiểm tra xem một mảng số có phải mảng giảm dần hay không
+// Mảng giảm dần nghĩa là các phần tử có giá trị giảm dần
+function isDescending(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm để loại bỏ các phần tử trùng nhau trong 1 mảng
+// Kết quả phản ảnh trực tiếp trong mảng đầu vào
+function removeDuplicate(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm đảo ngược thứ tự các phần tử trong 1 mảng
+// Kết quả phản ảnh trực tiếp trong mảng đầu vào
+function reverse(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm để lọc ra các số dương trong 1 mảng số
+// Kết quả trả về là một mảng mới
+function filterEvenNumber(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm kiểm tra xem một mảng có chứa bất kỳ số nào là số nguyên tố hay không
+function hasPrime(arr) {
+    // Code
+
+    return; // Kết quả
+}
+
+// Viết hàm để bình phương tất cả giá trị trong mảng
+// Kết quả trả về là một mảng mới
+function squareAllElement(arr) {
+    // Code
+
+    return; // Kết quả
+}
+Array.isArray(arr); // Kiểm tra giá trị có phải mảng
+arr.reverse(); // Đảo ngược mảng
+arr.includes(value); //Mảng có chứa value hay không
+arr.indexOf(value); //Chỉ mục của value(trái sang phải)
+arr.lastIndexOf(value); // Chỉ mục của value(phải qua trái)
+arr.pop(); // Xóa phần tử cuối cùng - trả về giá trị
+arr.push(value); //Thêm một giá trị vào cuối mảng
+arr.slice(from, to); // Sao chép các giá trị trong mảng
+arr.splice(from, to); //Xóa hoặc chèn phần tử
+arr.join(separtor);

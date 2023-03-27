afcd8c286a2493d740c384d22552dcc25c73a8e6
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-03-27 15:38:03 +0700

    Exercise Array JS

--- Exercise/ex1.js
+++ Exercise/ex1.js
@@ -1,23 +1,18 @@
 // Tính tổng các phần tử trong mảng số
 function sumOfArray(arr) {
   // Code
-  let sum = 0;
+  let sum = 0; // Khai báo biến
   for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+    sum += arr[i]; // Tính tổng các giá trị trong mảng
   }
   return sum; // Kết quả
 }
-// Goi ham sumOfArray()
+// Gọi hàm sumOfArray()
 console.log(sumOfArray([1, 3, 5, 7, 9]));
 // Tính trung bình cộng các số trong một mảng
 function averageOfArray(arr) {
   // Code
-  let sum = 0;
-  for (let i = 0; i < arr.length / 2; i++) {
-    sum += arr[i];
-  }
-
-  return sum / arr.length; // Kết quả
+  return sumOfArray(arr) / arr.length; // Kết quả
 }
 // Goi ham averageOfArray()
 console.log(averageOfArray([3, 4, 6, 5, 4]));
@@ -84,7 +79,7 @@ function removeDuplicate(arr) {
   for (let i = 0; i < doDai; i++) {
     for (let j = i + 1; j < doDai; j++) {
       if (arr[i] == arr[j]) {
-        splice(j, 1);
+        arr.splice(j, 1);
         doDai -= 1;
         j--;
       }
@@ -96,31 +91,50 @@ function removeDuplicate(arr) {
 console.log(removeDuplicate([1, 4, 6, 7, 4, 3]));
 // Viết hàm đảo ngược thứ tự các phần tử trong 1 mảng
 // Kết quả phản ảnh trực tiếp trong mảng đầu vào
-function reverse(arr) {
+function reverseArr(arr) {
   // Code
 
-  return; // Kết quả
+  return arr.reverse(); // Kết quả
 }
-
+// Goi ham reverseArr()
+console.log(reverseArr([1, 3, 4, 5, 6, 9]));
 // Viết hàm để lọc ra các số dương trong 1 mảng số
 // Kết quả trả về là một mảng mới
 function filterEvenNumber(arr) {
   // Code
 
-  return; // Kết quả
+  return arr.filter((number) => Math.sign(number) === 1); // Kết quả
 }
+// Goi ham filterEvenNumber()
+console.log(filterEvenNumber([1, -3, 4, 6, -5, -9]));
 
 // Viết hàm kiểm tra xem một mảng có chứa bất kỳ số nào là số nguyên tố hay không
+function primeNum(n) {
+  if (n < 2) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
 function hasPrime(arr) {
   // Code
-
-  return; // Kết quả
+  for (let i = 0; i < arr.length; i++) {
+    if (primeNum(arr[i])) return true;
+  }
+  return false; // Kết quả
 }
+// Gọi hàm hasPrime()
+console.log(hasPrime([1, 3, 5, 6, 7, 4, 9, 6]));
+console.log(hasPrime([1, 4, 6, 8, 9]));
 
 // Viết hàm để bình phương tất cả giá trị trong mảng
 // Kết quả trả về là một mảng mới
 function squareAllElement(arr) {
   // Code
 
-  return; // Kết quả
+  return arr.map((num) => num ** 2); // Kết quả trả về
 }
+// Gọi hàm squareAllElement()
+console.log(squareAllElement([1, 3, 5, 6])); // Kết quả : [ 1, 9, 25, 36 ]
+console.log(squareAllElement([2, 4, 6, 3])); // Kết quả :
